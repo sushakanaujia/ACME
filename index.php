a12f604d7f66a863bf3cbcf33cc6e51525d7ddc0
susha <susha.kanaujia@pixelconception.com>
2016-06-30 12:58:23 +0530

    update

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <html>
-<head>
-<meta charset="utf-8">
-        <title>ACME</title>
-        <!-- Mobile specific metas -->
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <!-- Force IE9 to render in normal mode -->
-        <!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
-        <meta name="author" content="" />
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
-        <meta name="application-name" content="" />
- <link href="css/style.css" rel="stylesheet" type="text/css">
- <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
-
- </head>
-<body>
-
-<div id="documentContainer">
+	<head>
+		<meta charset="utf-8">
+		<title>ACME</title>
+		<!-- Mobile specific metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<!-- Force IE9 to render in normal mode -->
+		<!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
+		<meta name="author" content="" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<meta name="application-name" content="" />
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+		<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div id="documentContainer">
 			<!-- wrap for header and page layout starts here -->
 			<div id="wrap">
 				<!-- companyHeader starts here -->
@@ -36,7 +34,7 @@
 					<div class="createContractPage pageContents">
 						<div class="container">
 							<div class="createEventInner customSelect">
-							<form action="process.php" method="post" class="form-horizontal" id="indexform" novalidate='novalidate'>
+								<form action="process.php" method="post" class="form-horizontal" id="indexform" novalidate='novalidate'>
 									<section class="createEventSection container_white">
 										<div class="pageSectionTitle">
 											ACME COMPUTER  information
@@ -87,7 +85,7 @@
 													</div>
 												</label>
 												<div class="col-md-4 col-sm-5 customTypeHead">
-												    <select id="vendor" name="vendor" style="width:360px;" onchange="clear_size();">
+													<select id="vendor" name="vendor" style="width:360px;" onchange="clear_size();">
 														<option value="">Select Your vendor Type</option>
 														<?php $vendor=array(1=>'Sony',2=>'Apple',3=>'Hp'); foreach($vendor as $val){?>
 														<option value="<?php echo lcfirst($val);?>"><?php echo ucfirst($val);?></option>
@@ -109,7 +107,7 @@
 													</div>
 												</label>
 												<div class="col-md-4 col-sm-5 customTypeHead">
-												    <select id="type" name="type" style="width:360px;" onchange="get_size();">
+													<select id="type" name="type" style="width:360px;" onchange="get_size();">
 														<option value="">Select Your Type</option>
 														<?php $type=array(1=>'laptop',2=>'personel desktop',3=>'server'); foreach($type as $val){?>
 														<option value="<?php echo lcfirst($val);?>"><?php echo ucfirst($val);?></option>
@@ -131,7 +129,7 @@
 													</div>
 												</label>
 												<div class="col-md-4 col-sm-5 customTypeHead">
-												    <select id="laptop_size" name="laptop_size" style="width:360px;">
+													<select id="laptop_size" name="laptop_size" style="width:360px;">
 														<option value="">Select Your Size</option>
 														<?php $size=array(1=>'small'); foreach($size as $val){?>
 														<option value="<?php echo lcfirst($val);?>"><?php echo ucfirst($val);?></option>
@@ -153,7 +151,7 @@
 													</div>
 												</label>
 												<div class="col-md-4 col-sm-5 customTypeHead">
-												    <select id="apple_size" name="apple_size" style="width:360px;">
+													<select id="apple_size" name="apple_size" style="width:360px;">
 														<option value="">Select Your Size</option>
 														<?php $size=array(1=>'small',2=>'medium'); foreach($size as $val){?>
 														<option value="<?php echo lcfirst($val);?>"><?php echo ucfirst($val);?></option>
@@ -175,7 +173,7 @@
 													</div>
 												</label>
 												<div class="col-md-4 col-sm-5 customTypeHead">
-												    <select id="server_size" name="server_size" style="width:360px;">
+													<select id="server_size" name="server_size" style="width:360px;">
 														<option value="">Select Your Size</option>
 														<?php $size=array(1=>'small',2=>'medium',3=>'large'); foreach($size as $val){?>
 														<option value="<?php echo lcfirst($val);?>"><?php echo ucfirst($val);?></option>
@@ -203,21 +201,21 @@
 				</div>
 				<!-- pageLayout ends here -->
 				<div id="confirmation" class="clearfix modal fade tradepointModal" role="dialog">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal">&times;</button>
-						<h4 class="modal-title">ACME COMPUTER REPAIR</h4>
-					</div>
-					<div class="modal-body">
-						<div class="comingSoonMsg">
-							Please select size
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal">&times;</button>
+								<h4 class="modal-title">ACME COMPUTER REPAIR</h4>
+							</div>
+							<div class="modal-body">
+								<div class="comingSoonMsg">
+									Please select size
+								</div>
+							</div>
+							
 						</div>
 					</div>
-					
 				</div>
-			</div>
-		</div>
 				<!-- for sticky footer -->
 				<div class="clearBoth"></div>
 				<div class="push"></div>
@@ -228,183 +226,166 @@
 			<!-- footer Container ends here -->
 			
 		</div>
-<script src="js/jquery-1.11.3.min.js"></script>
-<script src="js/jquery-migrate-1.2.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/validate.js"></script>
-<script src="js/validate.min.js"></script>
-
-
-
-<script>
-$(document).ready(function(){
-$('#name').val('');
-$('#mobile').val('');
-$('#vendor').val('');
-$('#type').val('');	
-var type=$('#type').val();
-var vendor=$('#vendor').val();
-if((vendor=='sony' || vendor=='apple' || vendor=='hp') && (type=='laptop' || type=='personel desktop')){
-	var lflag=true;
- }	else{ var lflag=false;}
-
-if((vendor=='sony' ||  vendor=='hp') && (type=='server')){
-	var sflag=true;
- }	else{ var sflag=false;} 
-
-if((vendor=='apple') && (type=='server')){
-	var aflag=true;
- }	else{ var aflag=false;} 
-$.validator.addMethod("blank", function(value, element) 
-{
-if((jQuery.trim(value)).length==0)
-  return false;
-else 
- return true;
-
-}, "Only blank spaces are  allowed");
-$.validator.addMethod("letters", function(value, element) 
-{    
-return this.optional(element) || /^[a-zA-Z\u00E0-\u00FC\:\-_#\n// ]+$/i.test(value);
-}, "Special characters and numbers are not allowed");
-$.validator.addMethod("validaddress", function(value, element) 
-{    
-return this.optional(element) || /^[a-zA-Z0-9\u00E0-\u00FC\.,:;\-_#$@&%`'?+"()\n// ]+$/i.test(value);
-}, "Special characters are not allowed"); 
-$.validator.addMethod("valid_commission", function(value, element) 
-{
-if(value >=1 && value <=100)
-  return true;
-else 
- return false;
-
-}, "Commission between 1-100 ");
-
-    $( "#indexform" ).validate(
-    {
-	highlight: function(element) {
-       if($(element).hasClass("chosen-select"))
-       {   
-        $(element).next("div").addClass("error");
-       }
-       else
-       {
-        $(element).addClass("error"); 
-       }
-       },
-      
-		errorPlacement: function(error, element) {
-			if(element.hasClass("chosen-select")){
-				error.insertAfter(element.parent().find('.chosen-container'));
-			}
-				else if(element.parents('.fl-content').find(".input-group-addon").length>0){
-						error.insertAfter(element.parent());
-				}
-				else if(element.hasClass("styled")){
-					error.insertAfter(element.parent());
-				}
-
-
-			else{
-					error.insertAfter(element);
-			}
-		  
+		<script src="js/jquery-1.11.3.min.js"></script>
+		<script src="js/jquery-migrate-1.2.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/validate.js"></script>
+		<script src="js/validate.min.js"></script>
+		<script>
+		$(document).ready(function(){
+		$('#name').val('');
+		$('#mobile').val('');
+		$('#vendor').val('');
+			$('#type').val('');
+		var type=$('#type').val();
+		var vendor=$('#vendor').val();
+		if((vendor=='sony' || vendor=='apple' || vendor=='hp') && (type=='laptop' || type=='personel desktop')){
+			var lflag=true;
+			}	else{ var lflag=false;}
+		if((vendor=='sony' ||  vendor=='hp') && (type=='server')){
+			var sflag=true;
+			}	else{ var sflag=false;}
+		if((vendor=='apple') && (type=='server')){
+			var aflag=true;
+			}	else{ var aflag=false;}
+		$.validator.addMethod("blank", function(value, element)
+		{
+		if((jQuery.trim(value)).length==0)
+		return false;
+		else
+		return true;
+		}, "Only blank spaces are  allowed");
+		$.validator.addMethod("letters", function(value, element)
+		{
+		return this.optional(element) || /^[a-zA-Z\u00E0-\u00FC\:\-_#\n// ]+$/i.test(value);
+		}, "Special characters and numbers are not allowed");
+		$.validator.addMethod("validaddress", function(value, element)
+		{
+		return this.optional(element) || /^[a-zA-Z0-9\u00E0-\u00FC\.,:;\-_#$@&%`'?+"()\n// ]+$/i.test(value);
+		}, "Special characters are not allowed");
+		$.validator.addMethod("valid_commission", function(value, element)
+		{
+		if(value >=1 && value <=100)
+		return true;
+		else
+		return false;
+		}, "Commission between 1-100 ");
+		$( "#indexform" ).validate(
+		{
+			highlight: function(element) {
+		if($(element).hasClass("chosen-select"))
+		{
+		$(element).next("div").addClass("error");
+		}
+		else
+		{
+		$(element).addClass("error");
+		}
 		},
-        rules: {
-           
-           
-	     vendor: {
-              
-                required: true,
-              
-            },
-        type: {
-              
-                required: true,
-              
-            }, 
-        laptop_size: {
-              
-                required: lflag,
-              
-            },
-        server_size: {
-              
-                required: sflag,
-              
-            }, 
-        apple_size: {
-              
-                required: aflag,
-              
-            },                
-        name:{
-              required: true,
-              blank: true,
-              letters: true,
-             },
-        mobile:{
-              required: true,
-              blank: true,
-              number: true,
-              
-        },
-            
-            
 		
-           
-        },
-        messages: {
-           
-           
-						
-        }
-    });
-$('#btncontracts').click(function() {
- 
-$('#indexform').valid();
-       if ($('#indexform').valid()) {
-       	if($('#laptop_size').val()=="" && $('#server_size').val()=="" && $('#server_size').val()==""){
- $('#confirmation').modal('show');
- return false;
- }else{
- 
- 
-	 $( "#indexform" ).submit();
-	}
-        
-        }   });
-
-});
-function get_size(){
-	
- var type=$('#type').val();
- var vendor=$('#vendor').val();
-
-if((vendor=='sony' || vendor=='apple' || vendor=='hp') && (type=='laptop' || type=='personel desktop')){
-$('#laptop').show();
-$('#apple').hide();
-$('#server').hide();
-
-}
-else if((vendor=='sony' ||  vendor=='hp') && (type=='server')){
-
-$('#laptop').hide();
-$('#apple').hide();
-$('#server').show();
-
-}
-else if((vendor=='apple') && (type=='server')){
-
-$('#laptop').hide();
-$('#server').hide();
-$('#apple').show();
-
-}
-}
-function clear_size(){
-$('#type').val('');
-}
-</script>
-</body>
-</html> 
+				errorPlacement: function(error, element) {
+					if(element.hasClass("chosen-select")){
+						error.insertAfter(element.parent().find('.chosen-container'));
+					}
+						else if(element.parents('.fl-content').find(".input-group-addon").length>0){
+								error.insertAfter(element.parent());
+						}
+						else if(element.hasClass("styled")){
+							error.insertAfter(element.parent());
+						}
+					else{
+							error.insertAfter(element);
+					}
+				
+				},
+		rules: {
+		
+		
+			vendor: {
+		
+		required: true,
+		
+		},
+		type: {
+		
+		required: true,
+		
+		},
+		laptop_size: {
+		
+		required: lflag,
+		
+		},
+		server_size: {
+		
+		required: sflag,
+		
+		},
+		apple_size: {
+		
+		required: aflag,
+		
+		},
+		name:{
+		required: true,
+		blank: true,
+		letters: true,
+		},
+		mobile:{
+		required: true,
+		blank: true,
+		number: true,
+		
+		},
+		
+		
+				
+		
+		},
+		messages: {
+		
+		
+								
+		}
+		});
+		$('#btncontracts').click(function() {
+		
+		$('#indexform').valid();
+		if ($('#indexform').valid()) {
+			if($('#laptop_size').val()=="" && $('#server_size').val()=="" && $('#server_size').val()==""){
+		$('#confirmation').modal('show');
+		return false;
+		}else{
+		
+		
+			$( "#indexform" ).submit();
+			}
+		
+		}   });
+		});
+		function get_size(){
+			
+		var type=$('#type').val();
+		var vendor=$('#vendor').val();
+		if((vendor=='sony' || vendor=='apple' || vendor=='hp') && (type=='laptop' || type=='personel desktop')){
+		$('#laptop').show();
+		$('#apple').hide();
+		$('#server').hide();
+		}
+		else if((vendor=='sony' ||  vendor=='hp') && (type=='server')){
+		$('#laptop').hide();
+		$('#apple').hide();
+		$('#server').show();
+		}
+		else if((vendor=='apple') && (type=='server')){
+		$('#laptop').hide();
+		$('#server').hide();
+		$('#apple').show();
+		}
+		}
+		function clear_size(){
+		$('#type').val('');
+		}
+		</script>
+	</body>
+</html>
--- process.php
+++ process.php
@@ -1,59 +1,56 @@
-<?php 
-
+<?php
 abstract class ACME
 {
-    public $repairarray=array(1=>'PC repairs',2=>'Doctor HP',3=>'Advance computing',4=>'Total Solution Computer');
-    public $repairing;
-    
-    public function repair($name,$mobile,$vendor,$type,$size)
-    {   
-        return $this->name . ", " . $this->mobile . ",".$this->vendor .",". $this->type.",". $this->size;    
-    }
-    
-    abstract public function touchscreen();
+public $repairarray=array(1=>'PC repairs',2=>'Doctor HP',3=>'Advance computing',4=>'Total Solution Computer');
+public $repairing;
+
+public function repair($name,$mobile,$vendor,$type,$size)
+{
+return $this->name . ", " . $this->mobile . ",".$this->vendor .",". $this->type.",". $this->size;
+}
+
+abstract public function touchscreen();
 }
 class services extends ACME
 {
-    public function touchscreen()
-    {
-        return "Touchscreen repairing Comming soon in future...!";    
-    }
+public function touchscreen()
+{
+return "Touchscreen repairing Comming soon in future...!";
+}
+
+public function repair($name,$mobile,$vendor,$type,$size)
+{
     
-    public function repair($name,$mobile,$vendor,$type,$size)
-    {   
-	
-	    if($this->vendor=='hp' && $this->type=='laptop' && ($this->size=='small' || $this->size=='medium' || $this->size=='large')){$repairing=$this->repairarray[1].' and '.$this->repairarray[2];}
-		if($this->vendor=='hp' && $this->type=='server' && ($this->size=='small' || $this->size=='medium' )){$repairing=$this->repairarray[2].' and '.$this->repairarray[4];}
-		if($this->vendor=='hp' && $this->type=='server' && ($this->size=='large' )){$repairing=$this->repairarray[2].' and '.$this->repairarray[4];}
-		if($this->vendor=='apple' && $this->type=='server' && $this->size=='medium'){$repairing=$this->repairarray[3];}
-		if($this->vendor=='hp' && $this->type=='server' && $this->size=='medium'){$repairing=$this->repairarray[2].' and '.$this->repairarray[3].' and '.$this->repairarray[4];}
-		if(($this->vendor=='sony' ||  $this->vendor=='apple') && ($this->type=='laptop' || $this->type=='personel desktop') && ($this->size=='small' || $this->size=='medium' || $this->size=='large')){$repairing=$this->repairarray[1];}
-		if(($this->vendor=='hp') && $this->type=='personel desktop' && ($this->size=='small' || $this->size=='medium' || $this->size=='large')){$repairing=$this->repairarray[1].' and '.$this->repairarray[2];}
-		if(($this->vendor=='sony') && $this->type=='server' && ($this->size=='medium' || $this->size=='small')){$repairing=$this->repairarray[3].' and '.$this->repairarray[4];}
-		if(($this->vendor=='sony') && $this->type=='server' && ($this->size=='large' )){$repairing=$this->repairarray[4];}
-		if(($this->vendor=='apple') && $this->type=='server' && ($this->size=='small' || $this->size=='medium' )){$repairing=$this->repairarray[3];}
-		if(($this->vendor=='apple') && $this->type=='server' && ($this->size=='large')){$repairing='server maintanance';}
-        return parent::repair($name,$mobile,$vendor,$type,$size) . ", for come for repairing under ".$repairing;    
-    }
+    if($this->vendor=='hp' && $this->type=='laptop' && ($this->size=='small' || $this->size=='medium' || $this->size=='large')){$repairing=$this->repairarray[1].' and '.$this->repairarray[2];}
+        if($this->vendor=='hp' && $this->type=='server' && ($this->size=='small' || $this->size=='medium' )){$repairing=$this->repairarray[2].' and '.$this->repairarray[4];}
+        if($this->vendor=='hp' && $this->type=='server' && ($this->size=='large' )){$repairing=$this->repairarray[2].' and '.$this->repairarray[4];}
+        if($this->vendor=='apple' && $this->type=='server' && $this->size=='medium'){$repairing=$this->repairarray[3];}
+        if($this->vendor=='hp' && $this->type=='server' && $this->size=='medium'){$repairing=$this->repairarray[2].' and '.$this->repairarray[3].' and '.$this->repairarray[4];}
+        if(($this->vendor=='sony' ||  $this->vendor=='apple') && ($this->type=='laptop' || $this->type=='personel desktop') && ($this->size=='small' || $this->size=='medium' || $this->size=='large')){$repairing=$this->repairarray[1];}
+        if(($this->vendor=='hp') && $this->type=='personel desktop' && ($this->size=='small' || $this->size=='medium' || $this->size=='large')){$repairing=$this->repairarray[1].' and '.$this->repairarray[2];}
+        if(($this->vendor=='sony') && $this->type=='server' && ($this->size=='medium' || $this->size=='small')){$repairing=$this->repairarray[3].' and '.$this->repairarray[4];}
+        if(($this->vendor=='sony') && $this->type=='server' && ($this->size=='large' )){$repairing=$this->repairarray[4];}
+        if(($this->vendor=='apple') && $this->type=='server' && ($this->size=='small' || $this->size=='medium' )){$repairing=$this->repairarray[3];}
+        if(($this->vendor=='apple') && $this->type=='server' && ($this->size=='large')){$repairing='server maintanance';}
+return parent::repair($name,$mobile,$vendor,$type,$size) . ", for come for repairing under ".$repairing;
+}
 }
 $com = new services();
 if($_POST['laptop_size']!=null){
-$_POST['size']=$_POST['laptop_size'];    
+$_POST['size']=$_POST['laptop_size'];
 }
 else if($_POST['server_size']!=null){
-$_POST['size']=$_POST['server_size'];    
+$_POST['size']=$_POST['server_size'];
 }
 else if($_POST['apple_size']!=null){
-$_POST['size']=$_POST['apple_size'];    
+$_POST['size']=$_POST['apple_size'];
 }
 $com->name = trim($_POST['name']);
 $com->mobile=trim($_POST['mobile']);
 $com->vendor=trim($_POST['vendor']);
 $com->type=trim($_POST['type']);
 $com->size=trim($_POST['size']);
-
 echo $com->repair($com->name,$com->mobile,$com->vendor,$com->type,$com->size);
 echo "<br/>";
 echo $com->touchscreen();
-
 ?>
